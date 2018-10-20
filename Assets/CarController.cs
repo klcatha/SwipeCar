9e271e7cccd3316664e3bf69cdf0da3969683551
klcatha <klcatha0518@gmail.com>
2018-10-20 17:26:26 +0900

    click car run

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+	float speed = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,12 @@ public class CarController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown(0)) {      //マウスがクリックされたら
+			this.speed = 0.2f;					// 初速度を設定
+		}
+
+		transform.Translate(this.speed, 0, 0);
+		this.speed *= 0.96f;
 		
 	}
 }
